a369891c08bb1a67433820a7da626f5b2704f3cd
roy <royrmagdaong@gmail.com>
2025-06-15 14:22:50 +0800

    post method, api params

--- index.js
+++ index.js
@@ -3,16 +3,49 @@ const cors = require('cors')
 const app = express()
 
 
+
 const port = 5000
 
+//middlewares
 app.use(cors())
+app.use(express.json()) //Notice express.json middleware
 
 
 app.get('/', (req, res) => {
    try {
         const pi = 3.14
-        pi = 1233;
         res.status(200).json({message: 'hello world', data: [1,2,3,4,5]})
+   } catch (error) {
+        res.status(500).json({message: error.message})
+   }
+})
+
+app.get('/:id', (req, res) => {
+   try {
+        const id = req.params.id
+        console.log('id', id)
+        // db query
+        // user info
+
+        res.status(200).json({message: 'hello world', data: [1,2,3,4,5]})
+   } catch (error) {
+        res.status(500).json({message: error.message})
+   }
+})
+
+app.post('/', (req, res) => {
+   try {
+        // console.log(req.body)
+        const username = req.body.username
+        const password = req.body.password
+        console.log('username', username)
+        console.log('password', password)
+
+        // if username exists
+        // password is correct
+        
+
+        res.status(200).json({message: 'success'})
    } catch (error) {
         res.status(500).json({message: error.message})
    }
