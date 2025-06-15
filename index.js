f3775e426d99250c45c5fa49441547b643446ba4
roy <royrmagdaong@gmail.com>
2025-06-15 14:47:03 +0800

    connection to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose');
 const app = express()
+
 
 const port = 5000
 
 //middlewares
 app.use(cors())
 app.use(express.json()) //Notice express.json middleware
+
+// connect to mongodb
+// Database connection
+mongoose.connect('mongodb://127.0.0.1:27017/')
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+
 
 // import routes
 const userRoutes = require('./routes/users')
